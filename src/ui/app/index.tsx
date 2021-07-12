33800e9fbdde0353e2f76397ce081454e48c58f6
Robb Traister <rtraister@citrine.io>
2021-07-12 17:38:15 -0400

    add layer toggle

--- src/ui/app/index.tsx
+++ src/ui/app/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Logo from "../../images/logo.svg";
 import { Map } from "../components/map";
 import { DiabetesLayer } from "../components/map/layers/diabetes";
@@ -6,11 +7,12 @@ import styles from "./index.scss";
 
 const mapConfig = {
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [-80, 33] as [number, number],
-  zoom: 7,
+  center: [-96, 38] as [number, number],
+  zoom: 4,
 };
 
 export function App() {
+  const [showDiabetes, setShowDiabetes] = useState(true);
   return (
     <>
       <div className={styles.header}>
@@ -18,7 +20,18 @@ export function App() {
         <span className={styles.wordMark}>Re:Public</span>
       </div>
       <Map config={mapConfig}>
-        <DiabetesLayer />
+        <div className={styles.toggle}>
+          <input
+            type="checkbox"
+            id="diabetes-toggle"
+            checked={showDiabetes}
+            onChange={(e) => {
+              setShowDiabetes(e.target.checked);
+            }}
+          />
+          <label htmlFor="diabetes-toggle">Diabetes Layer</label>
+        </div>
+        {showDiabetes ? <DiabetesLayer /> : null}
       </Map>
     </>
   );
